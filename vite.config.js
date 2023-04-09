1f55fc6e149544aca89ec7ed265cd1c51202f55e
AshrafElsaghier1 <ashrafelsaghier17@gmail.com>
2023-04-09 23:50:13 +0200

    publishing

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/swizz-energy/",
+  base: "/",
   build: {
     chunkSizeWarningLimit: 2800,
   },
