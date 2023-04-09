e2995ff822dd5e0ccf057c53008fe72a06026297
AshrafElsaghier1 <ashrafelsaghier17@gmail.com>
2023-04-09 23:35:04 +0200

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/swizz-energy/",
+  build: {
+    chunkSizeWarningLimit: 2800,
+  },
 })
